2f03fa26d6d2e4d4dfccbfe0f01daca403b37eb1
kylemoran138 <kylemoranp@gmail.com>
2018-11-08 18:53:03 -0500

    Made logic partially modular for future logic

--- AZEL.ino
+++ AZEL.ino
@@ -1,43 +1,78 @@
-int MTR_PN = 13;
-int DIR_PIN_ONE = 12;
-int DIR_PIN_TWO = 11;
-boolean DIR_EN_ONE = false;
-boolean DIR_EN_TWO = false;
-boolean MTR_EN = false;
+#include <math.h>  
+
+//To keep track of all arrays and the number of values
+const int NUM_SENSE_VALS = 2;
+const int NUM_CTRL_VALS = 3;
+
+// Control Pins
+// 0 = MTR_PN
+// 1 = DIR_PIN_ONE
+// 2 = DIR_PIN_TWO
+const int CTRL_PINS[NUM_CTRL_VALS] = {13, 12, 11};
+
+// Sensor pins
+// 0 = LEFT_PHOTO
+// 1 = RIGHT_PHOTO
+const int SENSE_PINS[NUM_SENSE_VALS] = {A0, A1};
+
+//Variables for motor state
+bool states[NUM_CTRL_VALS] = {false, false, false};
+float sensorValues[NUM_SENSE_VALS] = {0,0};
+
+//Variables for motor control
+bool reqStates[NUM_CTRL_VALS] = {false, false, false};
 
 void setup() {
-  pinMode(MTR_PN, OUTPUT);
-  pinMode(DIR_PIN_ONE, OUTPUT);
-  pinMode(DIR_PIN_TWO, OUTPUT);
-  digitalWrite(DIR_PIN_TWO, DIR_EN_TWO);
-  digitalWrite(DIR_PIN_ONE, DIR_EN_ONE);
-  digitalWrite(MTR_PN, MTR_EN);
+  for(int i = 0; i < NUM_CTRL_VALS-1; i++){
+    pinMode(CTRL_PINS[i], OUTPUT);
+  }
+
+  for(int i = 0; i < NUM_SENSE_VALS-1; i++){
+    pinMode(SENSE_PINS[i], OUTPUT);
+  }
+
+  for(int i = 0; i < NUM_CTRL_VALS-1; i++){
+    digitalWrite(CTRL_PINS[i], states[i]);
+  }
 }
 
 void loop() {
-  delay(3000);
-  setMotor(false);
-  setFwd(false);
-  delay(3000);
-  setMotor(true);
-  setFwd(false);
-  delay(3000);
-  setMotor(true);
-  setFwd(true);
+  senseData();
+
+  calculateRequirements();
+  // This is called at the end of the loop to react to input
+  reactToData();
 }
 
-void setMotor(bool EN){
-  MTR_EN = EN;
-  digitalWrite(MTR_PN, EN);
+void calculateRequirements(){
+  reqStates = {
+    ((sensorValues[0] % sensorValues[1]) != 0),
+    (sensorValues[0] > sensorValues[1]),
+    (sensorValues[0] < sensorValues[1])
+  };
+  //if(sensorValues[0] % sensorValues[1] != 0){
+  //  if(sensorValues[0] > sensorValues[1]){
+  //    reqStates = {true, true, false};
+  //  }else{
+  //    reqStates
+  //  }
+  //}else{
+  //  reqStates = {false, false, false};
+  //}
 }
 
-void setFwd(bool EN){
-  DIR_EN_ONE = EN;
-  DIR_EN_TWO = !EN;
-  digitalWrite(DIR_PIN_ONE, DIR_EN_ONE);
-  digitalWrite(DIR_PIN_TWO, DIR_EN_TWO);
-
+void senseData(){
+  for(int i = 0; i < NUM_SENSE_VALS-1; i++){
+    sensorValues[i] = analogRead(SENSE_PINS[i]);
+  }
 }
 
-
+void reactToData(){
+  for(int i = 0; i < NUM_CTRL_VALS-1; i++){
+    if(states[i] != reqStates[i]){
+      states[i] = reqStates[i];
+      digitalWrite(CTRL_PINS[i], states[i]);
+    }
+  }
+}
 
